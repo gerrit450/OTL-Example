755d3b20e5b86e26c912fb830f75327f80c14652
Gerrit van Zyl <Gerrit.vanzyl@myob.com>
2023-07-21 15:24:30 +1200

    Book model

---
+++ Librarian/Model/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace Librarian.Model
+{
+    public class Book
+    {
+        public Book(string name, string author)
+        {
+            Random rnd = new Random();
+
+            bookName = name;
+            bookId = rnd.Next(20);
+            bookAuthor = author;
+        }
+        public int bookId { get; }
+        public string bookName { get; }
+        public string bookAuthor { get; }
+    }
+}
