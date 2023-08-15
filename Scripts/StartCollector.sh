53b51e3a831de4e742df2fc1d5b0da5a449e25a8
Gerrit van Zyl <Gerrit.vanzyl@myob.com>
2023-08-15 13:04:45 +1200

    Modifed collector script and moved config in same directory. Problem is something with the docker volumes on wsl

--- Scripts/StartCollector.sh
+++ Scripts/StartCollector.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 # In a new window, run the otel collector on port 4317. [IN-PROGRESS]
-docker run -p 4317:4317 -v config.yaml:/etc/otelcol-contrib/config.yaml otel/opentelemetry-collector:latest
+#docker run --rm -p 4317:4317 -v config.yaml:/config.yaml otel/opentelemetry-collector-contrib:0.82.0 --config=config.yaml
+
+#docker run --rm -p 4317:4317 -v config.yaml:/etc/otelcol-contrib/config.yaml otel/opentelemetry-collector-contrib:0.82.0
+
+#docker run --rm -p 4317:4317 -v config.yaml:/etc/otelcol-contrib otel/opentelemetry-collector:latest --config=/etc/otelcol-contrib/config.yaml
+
+#docker run --rm -p 4317:4317 -v config.yaml:/etc/otelcol-contrib/config.yaml otel/opentelemetry-collector-contrib:latest
+
+docker run --rm -p 4317:4317 -p 9411 -v config.yaml:/etc/otelcol-contrib otel/opentelemetry-collector-contrib:latest --config=/etc/otelcol-contrib/config.yaml
+
