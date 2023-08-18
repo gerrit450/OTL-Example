a963d59c67be4d49fa2dd10db131bffd467423d7
Gerrit van Zyl <Gerrit.vanzyl@myob.com>
2023-08-18 14:13:06 +1200

    Moved list of books into its own class

---
+++ Librarian/Books.cs
@@ -0,0 +1,20 @@
+﻿using Librarian.Model;
+
+namespace Librarian
+{
+    internal static class Books
+    {
+        public static List<Book> ListOfBooks = new List<Book>()
+        {
+            new Book("book9","Smith"),
+            new Book("book1","James"),
+            new Book("book2","Betel"),
+            new Book("book3","Jack"),
+            new Book("book4","Gerrit"),
+            new Book("book5","Andrew"),
+            new Book("book6","Herbert"),
+            new Book("book7","Parminder"),
+            new Book("book8","Preet")
+        };
+    }
+}
